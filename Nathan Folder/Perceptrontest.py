c3584313ae7fa69303e3e4c1196f3cc89bcaab44
Tonella <2508690@stu.highlineschools.org>
2018-10-16 15:18:57 -0700

    Minor changes to comments, layout

--- Nathan Folder/Perceptrontest.py
+++ Nathan Folder/Perceptrontest.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 import os
 import random
-import time as t
+import time
 
 #Identifies inputs
 f = np.array([float(x) for x in input().split()])
@@ -18,16 +18,17 @@ b = -5
 #finds the sum of the weights*inputs, adds bias
 f1 = np.multiply(f, w)
 print(f1)
-t.sleep(2)
+time.sleep(2)
 
 f2 = np.sum(f1, axis=None) + b
 print(f2)
-t.sleep(2)
+time.sleep(2)
 
+#places sum into a sigmoid function
 f3 = 1/(1+(math.e**f2))
 print(f3)
 
-#threshold for activation (step function)
+#creates threshold to find output value (0 or 1)
 if f3 > 0.5:
     print('1')
 elif f3 <= 0.5:
