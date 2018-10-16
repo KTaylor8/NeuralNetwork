e1b0233a061a7ff3bb5939d718fece6d564ea2a8
Tonella <2508690@stu.highlineschools.org>
2018-10-16 14:17:45 -0700

    Starting to add Sigmoid Function

--- Nathan Folder/Perceptrontest.py
+++ Nathan Folder/Perceptrontest.py
@@ -4,17 +4,16 @@ import os
 import random
 import time as t
 
-
 #Identifies inputs
-f = np.array([int(x) for x in input().split()])
+f = np.array([float(x) for x in input().split()])
 print (f)
 l = f.size
 print (l)
 
 #creates the weight and biases
-w = np.random.randint(0, high=2, size=(1, l))
+w = np.random.randint(0, high=3, size=(1, l))
 print(w)
-b = -1
+b = -5
 
 #finds the sum of the weights*inputs, adds bias
 f1 = np.multiply(f, w)
@@ -25,8 +24,11 @@ f2 = np.sum(f1, axis=None) + b
 print(f2)
 t.sleep(2)
 
+f3 = 1/(1+(math.e**f2))
+print(f3)
+
 #threshold for activation (step function)
-if f2 > 0:
+if f3 > 0.5:
     print('1')
-elif f2 <= 0:
+elif f3 <= 0.5:
     print('0')
