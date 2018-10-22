d52f5a66264196c71c919bdb8d6e709cdad8c00b
Nathan T <nathantonella@gmail.com>
2018-10-21 22:49:04 -0700

    Cleaned up code

--- Nathan Folder/Perceptrontest.py
+++ Nathan Folder/Perceptrontest.py
@@ -4,32 +4,38 @@ import os
 import random
 import time
 
-#Identifies inputs
-f = np.array([float(x) for x in input().split()])
-print (f)
-l = f.size
-print (l)
+#define sigmoid function
+def sigmoid(x):
+    f3 = 1/(1+(math.e**((-1)*x)))
+    return f3
 
-#creates the weight and biases
-w = np.random.randint(0, high=3, size=(1, l))
-print(w)
-b = -5
+#define sum of dot product function
+def __sumofdot(x, w, b):
+    f1 = np.multiply(x, w)
+    f2 = np.sum(f1, axis=None) + b
+    return f2
 
-#finds the sum of the weights*inputs, adds bias
-f1 = np.multiply(f, w)
-print(f1)
-time.sleep(2)
+#define main code
+def main():
+    #Create list
+    f = np.array([float(x) for x in input().split()])
+    l = f.size
 
-f2 = np.sum(f1, axis=None) + b
-print(f2)
-time.sleep(2)
+    #assign weights and bias
+    w = np.random.randint(0, high=3, size=(1,l))
+    b = -5
 
-#places sum into a sigmoid function
-f3 = 1/(1+(math.e**((-1)*f2)))
-print(f3)
+    #find sum of dot product of list
+    f2 =__sumofdot(f, w, b)
+    
+    #apply activation function
+    f3 = sigmoid(f2)
 
-#creates threshold to find output value (0 or 1)
-if f3 > 0.5:
-    print('1')
-elif f3 <= 0.5:
-    print('0')
+    #set threshold and decide to fire or not
+    if f3 > 0.5:
+        print('1')
+    if f3 <= 0.5:
+        print('0')
+
+#run main code
+main()
