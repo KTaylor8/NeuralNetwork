ae090d798adf8ac34759ab2ee1786c372637f50c
Katie Taylor <katiemxtaylor@gmail.com>
2018-11-14 09:10:27 -0800

    fixing numpy

--- Katie/networkInitialization.py
+++ Katie/networkInitialization.py
@@ -9,23 +9,25 @@ import numpy as np
 import pandas as pd
 
 
-class Network():
-    with open("ticTacToeData.cvs", newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=' ')
-        for row in reader:
-            print('; '.join(row))
+def main():
+    print("done")
+    # class Network():
+    #     with open("ticTacToeData.cvs", newline='') as csvfile:
+    #         reader = csv.reader(csvfile, delimiter=' ')
+    #         for row in reader:
+    #             print('; '.join(row))
 
-# csv python file handling
-# split()
-# list of tuples w/ each row's data
+    # csv python file handling
+    # split()
+    # list of tuples w/ each row's data
 
-# # inputs file into python dictionary and converts it into list
-#     file = ('C:/Users/s-2508690/Desktop/NeuralNetwork/ticTacToeData.xlsx')
-#     x1 = pd.ExcelFile(file)
-#     df1 = x1.parse('Sheet1')  # con
+    # # inputs file into python dictionary and converts it into list
+    #     file = ('C:/Users/s-2508690/Desktop/NeuralNetwork/ticTacToeData.xlsx')
+    #     x1 = pd.ExcelFile(file)
+    #     df1 = x1.parse('Sheet1')  # con
 
-#     df1 = df1.values()  # converts into an array
-#     while row
+    #     df1 = df1.values()  # converts into an array
+    #     while row
 
 
 if __name__ == "__main__":
