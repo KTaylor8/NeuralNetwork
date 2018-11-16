1c60f323aa298b91df0d1bc4b4d683327f33452b
Stroganogg <l12345lemieux@gmail.com>
2018-11-16 11:24:44 -0800

    pretty graph v.2

--- Logan/prettygraph.py
+++ Logan/prettygraph.py
@@ -2,6 +2,15 @@ import matplotlib.pyplot as plt
 import math
 import pylab
 import numpy
+import csv
+
+with open("outputData.csv", newline='') as csvfile:
+    readData = csv.reader(csvfile, delimiter=' ')
+    for row in readData:
+        rowStr = "".join(row)
+        rowSplit = rowStr.split(",")
+        experimentalOutput = rowSplit[5]
+        print(experimentalOutput)
 
 x = numpy.linspace(-20,20,100)
 y = numpy.sin(x)
