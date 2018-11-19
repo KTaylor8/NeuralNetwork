4f96e05ee946c7e8792226cb7841166dd47e0781
Katie <katiemxtaylor@gmail.com>
2018-11-18 18:06:00 -0800

    this is where I stopped

--- Katie/networkInitialization.py
+++ Katie/networkInitialization.py
@@ -17,9 +17,9 @@ class network():
         self.layerSizes = layerSizes
         # self.b = [random.uniform(0, 1) for y in layerSizes[1:]]
         # self.w = [random.uniform(0, 1) for y in layerSizes[1:]]
-        self.b = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.b = [np.random.randn(y, 1) for y in layerSizes[1:]]
         self.w = [np.random.randn(y, x)
-                  for x, y in zip(layerSizes[:-1], sizes[1:])]  # ?
+                  for x, y in zip(layerSizes[:-1], layerSizes[1:])]  # ?
 
     def inputMinibatch(self):
         with open("ticTacToeData.csv", newline='') as csvfile:
@@ -27,50 +27,38 @@ class network():
             for minibatch in readData:  # each row begins as single el list
                 minibatchStr = "".join(minibatch)
                 minibatchSplit = minibatchStr.split(",")
-                # print(minibatchSplit)
                 theoreticalOutput = tuple(minibatchSplit[9])
                 minibatchInputs = tuple(minibatchSplit[0:8])
-                for i in minibatchInputs:
-                    # not sure how to assign number values to the inputs to make them calculatable in the sigmoid function
-                    feedforward(minibatchInputs[i])
-                # break
+                for inputNum in minibatchInputs:
+                    feedforward(minibatchInputs[inputNum], inputNum)
+                    # not sure how to assign number values to the inputs to make them calculatable in the sigmoid function; and not sure how to call the feedforward method
 
-    def feedforward(self, neuronInput):
-        """Runs a neuron input through all layers in a network and returns an output for it"""
-        for b, w in self.b, self.w:
-            neuronOutput = sigmoid(np.dot(w, neuronInput)+b)
-
-        dotProdSum = sumDotProd(f, w, b)
-        sigmoidOutput = sigmoid(dotProdSum)
+    def feedforward(self, neuronInput, inputNum):
+        """Runs a neuron input through all layers in a network and returns an output for the neuron"""
+        # I'M CONFUSED ABOUT HOW TO DO THE LOOPING SO THAT IT GOES THROUGH ALL NEURONS AND ALL LAYERS
+        # each input corresponds to a neuron in the hidden layer
+        for neuron in zip(self.b, self.w):
+            sigmoidOutput = sigmoid(np.dot(self.w[neuron],
+                                           neuronInput)+self.b[neuron])
 
         # compares sigmoidOutput to threshold to see if it fires or not:
+        # I'm not sure what the firing and not firing looks like in code
         if sigmoidOutput > 0.5:
-            print('1')
-        if sigmoidOutput <= 0.5:
-            print('0')
-        return neuronOutput
+            # fires and passes something on to outputs
+        elif sigmoidOutput <= 0.5:
+            # doesn't fire
+        return neuronOutput  # currently undefined
 
     """
-    The sigmoid activation function will map the raw output of inputs, weights, and biases onto a function, which can then be used to determine if a neuron fires or not.
+    The sigmoid activation function put the inputs, weights, and biases into a function that helps us determine if the neuron fiers or not.
     """
 
-    def sigmoid(x):
-        neuronOutput = 1/(1+(math.e**((-1)*x)))
-        return neuronOutput
-
-    """
-    The next step is to define the raw output function. This will take the sum of the dot product of the weights and inputs, then adds the bias.
-
-    A dot product is the product of two vectors (weights and inputs)
-    """
-
-    def sumDotProd(inputMinibatch, w, b):
-        dotProd = np.multiply(inputMinibatch, w)
-        dotProdSum = np.sum(dotProd, axis=None) + b
-        return dotProdSum
+    def sigmoid(self, x):
+        sigmoidOutput = 1/(1+(math.e**((-1)*x)))
+        return sigmoidOutput
 
 
-if __name__ == main():
+if __name__ == main():  # not sure how class relates to main()
     import doctest
     doctest.testmod()
     main()
