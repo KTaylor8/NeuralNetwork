db25a68ddbeea564e4725f11a4af28027475eadc
Katie <katiemxtaylor@gmail.com>
2019-03-06 11:09:59 -0800

    fixed messed up graph

---
+++ katieKerasTest.py
@@ -0,0 +1,21 @@
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+import numpy as np
+
+# For a single-input model with 2 classes (binary classification):
+
+model = Sequential()
+model.add(Dense(32, activation='relu', input_dim=100))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+# Generate dummy data
+data = np.random.random((10000, 100))
+labels = np.random.randint(2, size=(10000, 1))
+
+# Train the model, iterating on the data in batches of 32 samples
+model.fit(data, labels, epochs=10, batch_size=50)
+
+model.summary()
