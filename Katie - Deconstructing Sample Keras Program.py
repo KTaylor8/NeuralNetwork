7e01c604380ade959fc8b0d99bd807d0044a40ef
Katie <katiemxtaylor@gmail.com>
2019-03-26 11:38:30 -0700

    boop

--- Katie - Deconstructing Sample Keras Program.py
+++ Katie - Deconstructing Sample Keras Program.py
@@ -1,6 +1,6 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten
-from keras import optimizers
+from keras import optimizers, metrics
 import keras.utils
 import csv
 import numpy as np
@@ -9,21 +9,30 @@ model = Sequential()  # sets up network to be linear stack of layers
 
 # adds 32 layers with the activation function relu and 5 inputs
 
-model.add(Dense(9568, activation='relu', input_dim=5))
+model.add(Dense(1, activation='relu', input_dim=60))
 # adds 10 layers with the activation function softmax
 # model.add(Dense(10, activation='softmax'))
 # .compile() initializes network type
 model.compile(optimizer='SGD',  # this is the type of backprop
               loss='mean_squared_error',  # this is the type of cost function
-              metrics=['accuracy'])  # this is the fitness function to determine success
+              metrics=[metrics.binary_accuracy])  # this is the fitness function to determine success
 
-data = []
-with open(r"powerPlantData.csv", newline=''
+inputs = []
+outputs = []
+with open(r"sonar.all-data.csv", newline=''
           ) as dataFile:
     for row in dataFile:
         dataSplit = row.strip().split(",")
-        data.append(dataSplit)
-data = np.asarray((data))
+        for i in range(len(dataSplit)-1):
+            dataSplit[i] = float(dataSplit[i])
+            if dataSplit[-1] == 'M':  # theoretical output
+                dataSplit[-1] = 1.0
+            elif dataSplit[-1] == 'R':
+                dataSplit[-1] = 0.0
+        inputs.append(dataSplit[0:60])
+        outputs.append(dataSplit[60])
+data = np.asarray((inputs))
+labels = np.asarray((outputs))
 
 # labels = np.random.randint(10, size=(9000, 568)) #weights array
 # labels = np.random.uniform(low=-1, high=1, size=(9568, 9568))
@@ -46,6 +55,6 @@ data = np.asarray((data))
 # hot_labels = np.reshape(hot_labels, (9000, 568*1))
 
 # model.fit(training data, experimental output, batch_size=num samples per gradient update)
-model.fit(data, labels, epochs=5, batch_size=20)
+model.fit(data, labels, epochs=50, batch_size=20)
 
 model.summary()
