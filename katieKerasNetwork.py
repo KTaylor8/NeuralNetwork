3e3091feab2921e3b6fc3c0188dac609132aeea4
Katie <katiemxtaylor@gmail.com>
2019-03-27 11:16:38 -0700

    network that works!

--- katieKerasNetwork.py
+++ katieKerasNetwork.py
@@ -1,17 +1,15 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten
-from keras import optimizers, metrics
-import keras.utils
+from keras import optimizers, metrics, utils
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
 
 model = Sequential()  # sets up network to be linear stack of layers
 
-# adds 32 layers with the activation function relu and 5 inputs
-
 model.add(Dense(1, activation='relu', input_dim=60))
-# adds 10 layers with the activation function softmax
-# model.add(Dense(10, activation='softmax'))
+# adds a layer with the activation function relu, 60 inputs, and 1 output
+
 # .compile() initializes network type
 model.compile(optimizer='SGD',  # this is the type of backprop
               loss='mean_squared_error',  # this is the type of cost function
@@ -34,27 +32,27 @@ with open(r"sonar.all-data.csv", newline=''
 data = np.asarray((inputs))
 labels = np.asarray((outputs))
 
-# labels = np.random.randint(10, size=(9000, 568)) #weights array
-# labels = np.random.uniform(low=-1, high=1, size=(9568, 9568))
-
-
-# model.add(Flatten())  # reduces shape of labels (tuples)
-
-
-# outputs an array containing binary arrays (rows) that denote the presence (1.) or absence (0.) of a class (separate types of nums) in each index of the input array. # classes = # columns
-# Ex: # Inputting w/ an array of 5 labels out of a set of 3 classes {0, 1, 2}:
-# array([0, 2, 1, 2, 0])
-# output:
-# array([[ 1.,  0.,  0.],
-#        [ 0.,  0.,  1.],
-#        [ 0.,  1.,  0.],
-#        [ 0.,  0.,  1.],
-#        [ 1.,  0.,  0.]], dtype=float32)
-
-# hot_labels = keras.utils.to_categorical(labels)
-# hot_labels = np.reshape(hot_labels, (9000, 568*1))
-
 # model.fit(training data, experimental output, batch_size=num samples per gradient update)
-model.fit(data, labels, epochs=50, batch_size=20)
+
+history = model.fit(data, labels, validation_split=0.25,
+                    epochs=50, batch_size=16, verbose=1)
 
 model.summary()
+
+# Plot training & validation accuracy values
+plt.plot(history.history[metrics.binary_accuracy])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
