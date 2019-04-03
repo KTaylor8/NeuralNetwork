ad5e6b3b7ee1553dbe41942d84813fbcccdf309a
Katie <katiemxtaylor@gmail.com>
2019-04-03 11:32:36 -0700

    push push

--- histogramTest.py
+++ histogramTest.py
@@ -1,22 +1,35 @@
 from PIL import Image
 import numpy as np
+import glob
 
 
 def main():
-    inputs = []
-    try:
-        # Relative Path
-        img = Image.open(r"C:\Users\s-2508690\Desktop\NeuralNetwork\flower_0054.jpg")
-        
-        # Getting histogram of image
-        print(img.histogram())
-        inputs = img.histogram()
+    inputsList = []
+    for fileName in glob.glob('natural_images_stretched\\*.jpg'):
+        inputs = np.histogram(Image.open(fileName))
+        inputsList.append((fileName, inputs))
+    print(inputsList[0])
 
-    except IOError:
-        pass
-
-    inputs = np.asarray(inputs)
-    print(inputs)
 
 if __name__ == "__main__":
     main()
+
+
+# def main():
+#     inputs = []
+#     try:
+#         # Relative Path
+#         img = Image.open(r"C:\Users\s-2508690\Desktop\NeuralNetwork\flower_0054.jpg")
+
+#         # Getting histogram of image
+#         print(img.histogram())
+#         inputs = img.histogram()
+
+#     except IOError:
+#         pass
+
+#     inputs = np.asarray(inputs)
+#     print(inputs)
+
+# if __name__ == "__main__":
+#     main()
