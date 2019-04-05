b873d3b7eba41a6bf55ab68f0ef53da68e0a06ee
Katie <katiemxtaylor@gmail.com>
2019-04-05 11:57:11 -0700

    exported image data

--- Import_imagelist_test.py
+++ Import_imagelist_test.py
@@ -1,15 +1,18 @@
 from PIL import Image
 import glob
+import numpy as np
+
 
 def main():
     image_list = []
     for filename in glob.glob('natural_images_grayscale/*.jpg'):
-            img = Image.open(filename)
-            im = img.histogram()
-            print(im, "1")
-            image_list.append(img)
-            
+        img = Image.open(filename)
+        im = img.histogram()
+        print(im, "1")
+        image_list.append(img)
+
     print(image_list)
+
 
 if __name__ == "__main__":
     main()
