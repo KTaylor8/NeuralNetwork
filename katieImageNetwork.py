3b56ceeda7bc20535464593a2919236be15c15ef
Katie <katiemxtaylor@gmail.com>
2019-04-19 10:55:41 -0700

    scrambled data

--- katieImageNetwork.py
+++ katieImageNetwork.py
@@ -6,11 +6,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
+# doesn't actually work; it misses the 10.5% of photos that are airplanes because there are too many non-airplanes in the data and it just says everything is not an airplane
+
 
 def main():
     inputs = []
     outputs = []
-    with open(r"natural_images.csv", newline=''
+    with open(r"naturalImagesScrambled.csv", newline=''
               ) as dataFile:
         for row in dataFile:
             dataSplit = row.strip().split(",")
@@ -18,35 +20,32 @@ def main():
                 tOut = 1.0
             else:
                 tOut = 0.0
-            intensities = dataSplit[1].split(" ")
+            intensities = dataSplit[1:11]
             inputs.append(intensities)
             outputs.append(tOut)
-    data = np.asarray((inputs))
-    labels = np.asarray((outputs))
+    data = np.asarray(inputs)
+    labels = np.asarray(outputs)
 
-    # model = Sequential()  # sets up network to be linear stack of layers
+    model = Sequential()
 
-    # model.add(Dense(1, activation='tanh', input_dim=60))
-    # # adds a layer with the activation function relu, 60 inputs, and 1 output
+    model.add(Dense(1, activation='hard_sigmoid', input_dim=10))
 
-    # # .compile() initializes network type
-    # model.compile(optimizer='SGD',  # this is the type of backprop
-    #               loss='mean_squared_error',  # this is the type of cost function
-    #               metrics=[metrics.binary_accuracy])  # this is the fitness function to determine success
+    model.compile(optimizer='SGD',
+                  loss='mean_squared_error',
+                  metrics=[metrics.binary_accuracy])
 
-    # # model.fit(training data, experimental output, batch_size=num samples per gradient update (lower num means learns more quickly initially but doesn't increase maximum accuracy))
-    # history = model.fit(data, labels, epochs=5000, batch_size=1)
+    history = model.fit(data, labels, epochs=10, batch_size=1)
 
-    # model.summary()
+    model.summary()
 
-    # # Plot accuracy rates
-    # plt.plot(np.asarray(history.history["binary_accuracy"])*100)
-    # plt.title('Model accuracy')
-    # plt.ylabel('Accuracy')
-    # plt.xlabel('Epoch')
-    # axes = plt.gca()
-    # axes.set_ylim(40, 100)
-    # plt.show()
+    # Plot accuracy rates
+    plt.plot(np.asarray(history.history["binary_accuracy"])*100)
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    axes = plt.gca()
+    axes.set_ylim(40, 100)
+    plt.show()
 
 
 if __name__ == "__main__":
