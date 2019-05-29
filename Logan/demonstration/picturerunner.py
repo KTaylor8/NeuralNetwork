5a7d916535c8dcb5554fcaaa63064a6edcdf6504
Stroganogg <l12345lemieux@gmail.com>
2019-05-29 11:07:22 -0700

    working demo piece!!!!!

--- Logan/demonstration/picturerunner.py
+++ Logan/demonstration/picturerunner.py
@@ -1,16 +1,40 @@
-from keras import metrics
-from keras.models import model_from_yaml
-
-# load YAML and create model
-yaml_file = open('model.yaml', 'r')
-loaded_model_yaml = yaml_file.read()
-yaml_file.close()
-loaded_model = model_from_yaml(loaded_model_yaml)
-# load weights into new model
-loaded_model.load_weights("model.h5")
-print("Loaded model from disk")
+from numpy import loadtxt
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Activation, Flatten
+from keras.layers.advanced_activations import LeakyReLU, PReLU
+from keras import optimizers, metrics, utils
+from itertools import chain
+from PIL import Image
+import csv
+import numpy as np
  
-# evaluate loaded model on test data
-loaded_model.compile(loss='logcosh', optimizer='Adadelta', metrics=[metrics.binary_accuracy])
-score = loaded_model.evaluate(data, labels, verbose=0)
-print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
+model = load_model('model.h5')
+model.summary()
+fileName = input("What is the file name?")
+inputs = np.array(Image.open(str("output\\" + fileName + ".jpg")))
+inputs = list(chain.from_iterable(inputs))
+
+planeorperson = str(input("Is this a plane or a person?"))
+if str.lower(planeorperson) == "plane":
+    tOut = 1.0
+else:
+    tOut = 0.0
+
+with open("dataStorage.csv", "w") as dataStorage:
+    dataStorage.write(f"{tOut}, {inputs}\n")
+
+with open(r"dataStorage.csv", newline=''
+          ) as dataFile:
+    for row in dataFile:
+        dataSplit = row.strip().split(",")
+        intensities = []
+        for x in dataSplit[2:401]:
+            intensities.append(x)
+        intensities = intensities[::4]
+        intensities = [float(x) for x in intensities]
+        inputs.append(intensities)
+data = np.asarray(inputs)
+data = np.reshape((np.asarray(data[-1])),(1,100))
+
+score = model.evaluate((data), np.expand_dims(np.array(tOut),1), verbose=0)
+print("%s: %.2f%%" % (model.metrics_names[1], score[1]*100))
