1f124792c3f3dc4542c8f9b5483091ec6f710c0c
Katie <katiemxtaylor@gmail.com>
2019-05-31 12:00:46 -0700

    sorry I wanted to standardize the format

--- Logan/demonstration/picturerunner.py
+++ Logan/demonstration/picturerunner.py
@@ -7,15 +7,15 @@ from itertools import chain
 from PIL import Image
 import csv
 import numpy as np
- 
+
 model = load_model('model.h5')
 model.summary()
-fileName = input("What is the file name?")
-inputs = np.array(Image.open(str("output\\" + fileName + ".jpg")))
+fileName = input("What is the file name? ")
+inputs = np.array(Image.open(f"output\\{fileName}.jpg"))
 inputs = list(chain.from_iterable(inputs))
 
-planeorperson = str(input("Is this a plane or a person?"))
-if str.lower(planeorperson) == "plane":
+planeorperson = input("Is this a plane or a person? ")
+if planeorperson.lower() == "plane":
     tOut = 1.0
 else:
     tOut = 0.0
@@ -34,7 +34,17 @@ with open(r"dataStorage.csv", newline=''
         intensities = [float(x) for x in intensities]
         inputs.append(intensities)
 data = np.asarray(inputs)
-data = np.reshape((np.asarray(data[-1])),(1,100))
+data = np.reshape((np.asarray(data[-1])), (1, 100))
 
-score = model.evaluate((data), np.expand_dims(np.array(tOut),1), verbose=0)
+score = model.evaluate((data), np.expand_dims(np.array(tOut), 1), verbose=0)
 print("%s: %.2f%%" % (model.metrics_names[1], score[1]*100))
+if score[1] == 1:
+    if planeorperson == "plane":
+        print("This is a plane!")
+    elif planeorperson == "person":
+        print("This is a person!")
+elif score[1] == 0:
+    if planeorperson == "plane":
+        print("This is a person!")
+    elif planeorperson == "person":
+        print("This is a plane!")
